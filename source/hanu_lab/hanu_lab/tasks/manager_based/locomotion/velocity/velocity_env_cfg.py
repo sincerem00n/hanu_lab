fe882af6f59e23b31f60aa92c97cd5a9991d3286
sincerem00n <jingjaijan62@gmail.com>
2025-12-15 15:32:51 +0700

    edit: preserve joint order

--- source/hanu_lab/hanu_lab/tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
+++ source/hanu_lab/hanu_lab/tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
@@ -109,7 +109,8 @@ class CommandsCfg:
 class ActionsCfg:
     """Action specifications for the MDP."""
 
-    joint_pos = mdp.JointPositionActionCfg(asset_name="robot", joint_names=[".*"], scale=0.5, use_default_offset=True)
+    joint_pos = mdp.JointPositionActionCfg(asset_name="robot", joint_names=[".*"], scale=0.5, use_default_offset=True, preserve_order=True
+    )
 
 
 @configclass
