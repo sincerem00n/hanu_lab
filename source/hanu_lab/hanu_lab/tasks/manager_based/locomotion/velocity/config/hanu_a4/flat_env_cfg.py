acd780cda59e672614223c0bf62199d7d9d0b750
sincerem00n <jingjaijan62@gmail.com>
2026-03-31 19:04:36 +0700

    add: task 6 for new delay

--- source/hanu_lab/hanu_lab/tasks/manager_based/locomotion/velocity/config/hanu_a4/flat_env_cfg.py
+++ source/hanu_lab/hanu_lab/tasks/manager_based/locomotion/velocity/config/hanu_a4/flat_env_cfg.py
@@ -6,7 +6,7 @@
 from isaaclab.managers import SceneEntityCfg
 from isaaclab.utils import configclass
 
-from .rough_env_cfg import HanuA4RoughEnvCfgV0, HanuA4RoughEnvCfgV1, HanuA4RoughEnvCfgV2, HanuA4RoughEnvCfgV3, HanuA4RoughEnvCfgV4, HanuA4RoughEnvCfgV5
+from .rough_env_cfg import HanuA4RoughEnvCfgV0, HanuA4RoughEnvCfgV1, HanuA4RoughEnvCfgV2, HanuA4RoughEnvCfgV3, HanuA4RoughEnvCfgV4, HanuA4RoughEnvCfgV5, HanuA4RoughEnvCfgV6
 
 @configclass
 class HanuA4FlatEnvCfgV0(HanuA4RoughEnvCfgV0):
@@ -90,6 +90,21 @@ class HanuA4FlatEnvCfgV4(HanuA4RoughEnvCfgV4):
 @configclass
 class HanuA4FlatEnvCfgV5(HanuA4RoughEnvCfgV5):
     """Configuration for the flat environment in the RAI Hanumanoid project. -- target q + Imu"""
+    def __post_init__(self):
+        super().__post_init__()
+
+        self.scene.terrain.terrain_type = "plane"
+        self.scene.terrain.terrain_generator = None
+
+        self.scene.height_scanner = None
+        self.observations.policy.height_scan = None
+        self.observations.critic.height_scan = None
+
+        self.curriculum.terrain_levels = None
+
+@configclass
+class HanuA4FlatEnvCfgV6(HanuA4RoughEnvCfgV6):
+    """Configuration for the flat environment in the RAI Hanumanoid project. -- delay , history length"""
     def __post_init__(self):
         super().__post_init__()
 
